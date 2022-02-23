1a61a012ddcad2c0eda7c566ed4ad73c280bbbc4
Artem Alexeyenko <i.html06@gmail.com>
2022-02-23 21:54:45 +0600

    feat: adaptive all sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-  $('.inform-slider').slick();
-});
+function burgerMenu(selector) {
+  const menu = document.querySelector(selector);
+  const button = document.querySelector('.header-nav__burger');
+  const links = document.querySelectorAll('.header-nav__menu-link');
+  const overlay = document.querySelector('.header-nav__overlay');
+
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  })
+  links.addEventListener('click', toggleMenu());
+  overlay.addEventListener('click', toggleMenu());
+
+  function toggleMenu() {
+    button.classList.toggle('header-nav__burger_active');
+    if(button.classList.contains('header-nav__burger_active')) {
+      menu.style.right = 0;
+    } else {
+      menu.style.right = '-200%';
+    }
+  }
+}
+burgerMenu('.header-nav__menu');
